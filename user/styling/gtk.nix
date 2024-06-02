f980ca11d3fcdf26c0ec6831283a1e6c71515e29
jordanaq <quinn.jordan@proton.me>
2024-06-02 16:47:37 -0700

    Commented out cursorTheme to test gtk

--- user/styling/gtk.nix
+++ user/styling/gtk.nix
@@ -1,14 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  home.pointerCurser = {
+  home.pointerCursor = {
     gtk.enable = true;
+    name = "Catppuccin Macchiato";
     package = pkgs.catppuccin-cursors.macchiatoPink;
     size = 16;
   };
 
   gtk = {
     enable = true;
-    cursorTheme = pkgs.catppuccin-cursors.macchiatoPink;
+    #cursorTheme = pkgs.catppuccin-cursors.macchiatoPink;
   };
+
+  home.packages = with pkgs; [
+    pkgs.dconf
+  ];
 }
