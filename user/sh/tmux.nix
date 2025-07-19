a397f8f880acc1ece05896cffa9a4656032115a9
jordanaq <quinn.jordan@protonmail.ch>
2025-07-19 14:29:24 -0400

    Updated tmux config

--- user/sh/tmux.nix
+++ user/sh/tmux.nix
@@ -4,5 +4,27 @@
   programs.tmux = {
     enable = true;
     shell = "\${pkgs.fish}/bin/fish}";
+
+    terminal = "tmux-256color";
+
+    historyLimit = 100000;
+
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      catppuccin
+    ];
+
+    extraConfig = ''
+      # Enable truecolor support
+      set-option -ga terminal-overrides ",xterm-256color:Tc"
+
+      # Other useful options
+      set-option -g mouse on
+
+      # set-option -g history-limit 10000
+      setw -g automatic-rename on
+
+      set -g @catppuccin_flavor "macchiato"
+    '';
   };
 }
