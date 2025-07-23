b41c6f59d368e5282b379a27f452cfa2e2b40394
jordanaq <quinn.jordan@protonmail.ch>
2025-07-23 16:43:35 -0400

    Added catppuccin, copilot, and vim plugins with some config

--- user/programming/vscode/default.nix
+++ user/programming/vscode/default.nix
@@ -6,17 +6,32 @@
     profiles = {
       default = {
         extensions = with pkgs.vscode-extensions; [
+          catppuccin.catppuccin-vsc
+          catppuccin.catppuccin-vsc-icons
+
+          github.copilot
+          github.copilot-chat
+
           ms-python.python
-          # ms-python.pylint
-          # ms-python.black-formatter
-          # ms-python.vscode-pylance
+          ms-python.pylint
+          ms-python.black-formatter
+          ms-python.vscode-pylance
 
           ms-toolsai.jupyter
 
-          tomoki1207.pdf
-
           ms-vsliveshare.vsliveshare
+
+          tomoki1207.pdf
+          
+          vscodevim.vim
         ];
+
+        userSettings = {
+          "vim.useSystemClipboard" = true;
+          "vim.hlsearch" = true;
+          "workbench.colorTheme" = "Catppuccin Macchiato";
+          "workbench.iconTheme" = "catppuccin";
+        };
       };
     };
   };
