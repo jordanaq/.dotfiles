f2cfafb92d08d4f39e7f0af164058b5a76fe261f
jordanaq <quinn.jordan@protonmail.ch>
2026-01-03 19:49:32 -0500

    Added thunderbird, calibre, and kdenlive

--- user/media/misc.nix
+++ user/media/misc.nix
@@ -5,7 +5,16 @@
     enable = true;
   };
 
+  programs.thunderbird = {
+    enable = true;
+    profiles.default = {
+      isDefault = true;
+    };
+  };
+
   home.packages = with pkgs; [
+    calibre
+    kdePackages.kdenlive
     obs-studio
     vlc
     zathura
