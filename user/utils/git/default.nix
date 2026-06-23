9a1137f58df76974f51f17349c446d175bff09b1
jordanaq <quinn.jordan@protonmail.ch>
2026-06-23 01:46:54 -0400

    Updated to new settings option for git

--- user/utils/git/default.nix
+++ user/utils/git/default.nix
@@ -6,24 +6,26 @@
 
     lfs.enable = true;
 
-    userName = "jordanaq";
-    userEmail = "quinn.jordan@protonmail.ch";
+    settings = {
+      user.name = "jordanaq";
+      user.email = "quinn.jordan@protonmail.ch";
 
-    aliases = {
-      a = "add";
-      ca = "commit -a";
-      cam = "commit -am";
-      ch = "checkout";
-      co = "commit";
-      cm = "commit -m";
-      com = "commit -m";
-      d = "diff";
-      pl = "pull";
-      ps = "push";
-      ro = "restore";
-      s = "status";
-      smu = "submodule update --init --recursive";
-      st = "status";
+      alias = {
+        a = "add";
+        ca = "commit -a";
+        cam = "commit -am";
+        ch = "checkout";
+        co = "commit";
+        cm = "commit -m";
+        com = "commit -m";
+        d = "diff";
+        pl = "pull";
+        ps = "push";
+        ro = "restore";
+        s = "status";
+        smu = "submodule update --init --recursive";
+        st = "status";
+      };
     };
   };
 }
