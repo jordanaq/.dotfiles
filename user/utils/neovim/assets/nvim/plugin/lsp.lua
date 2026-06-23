c4d41509f10684e813d83d1f9544c75d60765faf
jordanaq <quinn.jordan@protonmail.ch>
2026-06-23 02:21:25 -0400

    Updated to newest lsp api

--- user/utils/neovim/assets/nvim/plugin/lsp.lua
+++ user/utils/neovim/assets/nvim/plugin/lsp.lua
@@ -1,70 +1,18 @@
---local on_attach = function(_, bufnr)
---
---  local bufmap = function(keys, func)
---    vim.keymap.set('n', keys, func, { buffer = bufnr })
---  end
---
---  bufmap('<leader>r', vim.lsp.buf.rename)
---  bufmap('<leader>a', vim.lsp.buf.code_action)
---
---  bufmap('gd', vim.lsp.buf.definition)
---  bufmap('gD', vim.lsp.buf.declaration)
---  bufmap('gI', vim.lsp.buf.implementation)
---  bufmap('<leader>D', vim.lsp.buf.type_definition)
---
---  bufmap('gr', require('telescope.builtin').lsp_references)
---  bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
---  bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
---
---  bufmap('K', vim.lsp.buf.hover)
---
---  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
---    vim.lsp.buf.format()
---  end, {})
---end
---
---local capabilities = vim.lsp.protocol.make_client_capabilities()
---capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-local lsp_zero = require("lsp-zero")
-lsp_zero.preset('recommended')
+vim.lsp.config("*", {
+  capabilities = capabilities,
+})
 
-lsp_zero.extend_lspconfig("harper_ls", {
+vim.lsp.config("harper_ls", {
   settings = {
     harper_ls = {
       userDictPath = "~/dict.txt",
-      filetypes = {
-        "c",
-        "cpp",
-        "cs",
-        "gitcommit",
-        "go",
-        "html",
-        "java",
-        "javascript",
-        "lua",
-        "markdown",
-        "nix",
-        "python",
-        "ruby",
-        "rust",
-        "sml",
-        "swift",
-        "tex",
-        "toml",
-        "typescript",
-        "typescriptreact",
-        "haskell",
-        "cmake",
-        "typst",
-        "php",
-        "dart"
-      }
-    }
+    },
   },
 })
 
-lsp_zero.extend_lspconfig("ltex-plus", {
+vim.lsp.config("ltex_plus", {
   settings = {
     ltex = {
       checkFrequency = "save",
@@ -73,48 +21,50 @@ lsp_zero.extend_lspconfig("ltex-plus", {
   },
 })
 
-lsp_zero.setup_servers({
-  'lua_ls',
-  'ltex-plus',
-  'harper_ls',
-  'hls',
-  'millet',
-  'pyright',
-  'tsserver',
-  'vale_ls',
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(event)
+    local bufnr = event.buf
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+    local function bufmap(keys, func)
+      vim.keymap.set("n", keys, func, { buffer = bufnr })
+    end
+
+    bufmap("<leader>r", vim.lsp.buf.rename)
+    bufmap("<leader>a", vim.lsp.buf.code_action)
+
+    bufmap("gd", vim.lsp.buf.definition)
+    bufmap("gD", vim.lsp.buf.declaration)
+    bufmap("gI", vim.lsp.buf.implementation)
+    bufmap("<leader>D", vim.lsp.buf.type_definition)
+
+    bufmap("gr", require("telescope.builtin").lsp_references)
+    bufmap("<leader>s", require("telescope.builtin").lsp_document_symbols)
+    bufmap("<leader>S", require("telescope.builtin").lsp_dynamic_workspace_symbols)
+
+    bufmap("K", vim.lsp.buf.hover)
+
+    vim.api.nvim_buf_create_user_command(bufnr, "Format", function()
+      vim.lsp.buf.format()
+    end, { force = true })
+
+    if client and client.name:match("ltex") then
+      require("ltex_extra").setup({
+        load_langs = { "en-US" },
+        path = ".ltex",
+      })
+    end
+  end,
 })
 
-lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({ buffer = bufnr })
-
-  if client.name:match("ltex") then
-    require("ltex_extra").setup({
-      load_langs = { "en-US" },
-      path = ".ltex",
-    })
-  end
-end)
-
-lsp_zero.setup()
-
---require('neodev').setup()
---require('lspconfig').lua_ls.setup {
---  on_attach = on_attach,
---  capabilities = capabilities,
---    root_dir = function()
---      return vim.loop.cwd()
---  end,
---    cmd = { "lua-language-server" },
---  settings = {
---      Lua = {
---          workspace = { checkThirdParty = false },
---          telemetry = { enable = false },
---      },
---  }
---}
---
---require('lspconfig').nil.setup {
---    on_attach = on_attach,
---    capabilities = capabilities,
---    cmd = { "nil" },
---}
+vim.lsp.enable({
+  "lua_ls",
+  "ltex_plus",
+  "harper_ls",
+  "hls",
+  "millet",
+  "nil_ls",
+  "pyright",
+  "ts_ls",
+  "vale_ls",
+})
