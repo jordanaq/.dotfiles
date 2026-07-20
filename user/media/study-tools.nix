5e7c52bbcd6aa66da09be9014f1cead6a3c308b1
jordanaq <quinn.jordan@protonmail.ch>
2026-07-20 07:15:59 -0400

    Added a wrapper for obsidian cli and changed desktop file for obsidian cmd exec

--- user/media/study-tools.nix
+++ user/media/study-tools.nix
@@ -1,8 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   home.packages = with pkgs; [
     zotero
     obsidian
+    (lib.hiPrio (writeShellScriptBin "obsidian" ''
+      exec ${obsidian}/bin/obsidian-cli "$@"
+    ''))
   ];
+
+  # Keep `obsidian` as the command/IPC CLI used by command rules, but make the
+  # graphical desktop launcher invoke Electron directly.
+  xdg.desktopEntries.obsidian = {
+    name = "Obsidian";
+    comment = "Knowledge base";
+    exec = "${pkgs.obsidian}/bin/obsidian %U";
+    icon = "obsidian";
+    categories = [ "Office" ];
+    mimeType = [ "x-scheme-handler/obsidian" ];
+  };
 }
