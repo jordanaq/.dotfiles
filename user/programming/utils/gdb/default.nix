85907c1f5c0d2533e757c80b26653f28493ef3e0
jordanaq <quinn.jordan@protonmail.ch>
2026-07-27 09:53:22 -0400

    Added gdb carveout for projects

--- user/programming/utils/gdb/default.nix
+++ user/programming/utils/gdb/default.nix
@@ -3,6 +3,9 @@
 {
   home.file.".gdbinit".text = ''
     add-auto-load-safe-path /nix/store
+    add-auto-load-safe-path /home/tsiru/Documents/Projects
+    add-auto-load-safe-path /home/tsiru/Documents/Projects/*
+
   '';
 
   home.packages = with pkgs; [
