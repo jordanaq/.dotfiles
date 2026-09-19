8e199ff4a48440830ca2b0e8e299349a89c9ce9c
jordanaq <quinn.jordan@protonmail.ch>
2026-09-19 19:43:03 -0400

    Added echo test

--- system/backup/restic.nix
+++ system/backup/restic.nix
@@ -63,6 +63,8 @@
     script = ''
       token="$(cat "$CREDENTIALS_DIRECTORY/push-token")"
 
+      echo "http://127.0.0.1:3001/api/push/$token"
+
       ${pkgs.curl}/bin/curl \
         --fail \
         --silent \
